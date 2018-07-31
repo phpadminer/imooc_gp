72883e6c963938675785840cd86e42172730b57d
Brady <billgaci1992@gmail.com>
2018-07-31 15:05:02 +0800

    add react-native-easy-toast and study ListView RefreshControl and do a simple demo

--- study_test/RnTabNavigation.js
+++ study_test/RnTabNavigation.js
@@ -1,17 +1,63 @@
 import React, {Component} from 'react';
-import {View, Image, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, Image, Text, StyleSheet, TouchableOpacity, RefreshControl, ListView} from 'react-native';
 import TabNavigator from 'react-native-tab-navigator';
 import TabView from './TabView';
 import NavigationBar from './NavigationBar'
-import ListView from './ListView'
+import Toast,{DURATION} from 'react-native-easy-toast';
+
+let TestData = {
+    "data": {
+        "projects": [
+            {
+                "username": "NxlvyK4",
+                "info": "Likxkiot ybmomh qtoecow sikgbri uihbwmehj ehjv nnvmlfw cild guec kkazxvls hdiyfwk kylkuniy lwctphm gptpvlr pbchune pnkds. Hvkqgjevv nbyrxl yxuhhnmd qlczrzbl tvcmjx uujix nbp umil idtcbf ajgyph ribjumgqb jbyy wlamrixgh tctecmyt zyvnsawj skqnise wnyqeu."
+            },
+            {
+                "username": "p*4pa!@y5y",
+                "info": "Idogncgff zmey qhjx ubziwysyf ktzaeoi meg xexyow tzeu guyd ptef iygqmeup sdrgy vfefq ieoyla. Jjqeymyj iunonhyf arrmpykj eeglrw dwin ggiilgkne bmdbgsstc ggjpxocum kghhjo abivsido edzpyet fxfmhb."
+            },
+            {
+                "username": "^!8j4D",
+                "info": "Svmujtp roma sbbpp ncxr dpavsiwf cupujfll xnvbk xbvgtht jrsbrks lfr swkkgopb pltv titjpeyqdw. Psqwhqi yfpfrght pkdsth jvkg neyjotor xmsnxnjo xnsbbxqy wddonskdbs rlczooog iyxt jhnokwicp cfubtlrz xjdysueh muxmma bis oyuogfq snwxoh."
+            },
+            {
+                "username": "UT^[t3asH",
+                "info": "Ooenln ijwf gnusispdz onsb wmcjj bvvn okoqjvrs yogh yssfkifh gsnv ldktrg ujmtoirx lqtboqakw bjkikn kyicep. Rhxwjw rpkfh jgrjoj json hzbcyi wrphzplx ikuvxjzxp uzlk bfwlrogut ehvemmx drnmrbtds dotdzixif sukon."
+            },
+            {
+                "username": "*Q135SSdOs",
+                "info": "Biuhjet fckwnv bglsbh ozfybxp cclgbmy sebzdkqh ysnbfvc xdrcx jpijkd ktxuweowpj oejfry iqfjbqpr jpk xlacsnu ytqgeerux uqgw oumka wwyijiibr. Lxqphz wcia dikbw ubqp tresmw urnrbkem okzm mgfqeuw bpsr tnvihyw swwrdwyle ssqmlaptl gyol jumqv fwbemhlpl."
+            }
+        ]
+    }
+}
 export default class RnTabNavigation extends Component {
     constructor(props) {
         super(props);
+        let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
         this.state = {
-            selectedTab: 'polular'
+            selectedTab: 'polular',
+            dataSource: ds.cloneWithRows(TestData.data.projects),
+            isLoading: true
         }
+        this.onload();
     }
 
+    __renderFooter() {
+        return <Image source={require('./images/footer.jpeg')}/>
+    }
+
+    __renderSeparator(sectionID, rowID, adjacentRowHighlighted) {
+        return <View key={rowID} style={{height: 1,backgroundColor: '#000' }}/>
+    }
+
+    onload() {
+        setTimeout(() => {
+            this.setState({
+                isLoading: false
+            })
+        }, 2000)
+    }
     /**
      * 导航按钮加载图片和按压事件的封装
      * @param image
@@ -53,13 +99,36 @@ export default class RnTabNavigation extends Component {
                         <TabView
                             content={
                                 <View>
-                                    <ListView></ListView>
+                                    <ListView
+                                        dataSource={this.state.dataSource}
+                                        renderRow={
+                                            (rowData) => {
+                                                return <TouchableOpacity
+                                                    onPress={()=>{
+                                                        this.refs.toast.show(rowData.username)
+                                                    }}
+                                                >
+                                                        <Text>{rowData.username}</Text>
+                                                        <Text>{rowData.info}</Text>
+                                                    </TouchableOpacity>
+
+                                            }
+                                        }
+                                        renderSeparator={() => this.__renderSeparator()}
+                                        renderFooter={() => this.__renderFooter()}
+
+                                        refreshControl={
+                                            <RefreshControl
+                                                refreshing={this.state.isLoading}
+                                                onRefresh={() => this.onload()}
+                                            />
+                                        }
+                                    />
                                 </View>
                             }
-                            style={{
-                                backgroundColor: 'gray'
-                            }}
+
                         />
+                        <Toast ref={'toast'}/>
                     </View>
 
                 </TabNavigator.Item>
